ee56881b162bb03cecaa5e91393bb5ef63a901f9
Nick Lobanov <ne.ice.evil@gmail.com>
2018-04-28 04:34:29 +0300

    no message

--- lib/crc.ex
+++ lib/crc.ex
@@ -9,6 +9,10 @@ defmodule CRC do
     crc8_calculation(data, 0x31, 0x00, true, true, 0x00)
   end
 
+  def crc8_cdma2000(data) do
+    crc8_calculation(data, 0x9B, 0xFF, false, false, 0x00)
+  end
+
   defp crc8_calculation(<<first_byte :: size(8), other_bytes :: binary>>, polynomial, initial, ref_in, ref_out, xor_out) do
     crc_byte = initial ^^^ if ref_in do reverce_byte(first_byte) else first_byte end
     crc_byte = crc8_loop(crc_byte, polynomial, ref_out)
@@ -21,27 +25,15 @@ defmodule CRC do
 
   defp crc8_loop(crc_byte, poly, ref_out, counter \\ 0)
   defp crc8_loop(crc_byte, poly, ref_out, counter) when counter < 8 do
-    crc_byte = if ((crc_byte &&& 0x80) != 0) do
-      crc_byte = :math.fmod((crc_byte <<< 1), 256)
-      crc_byte = round(crc_byte)
-      if ref_out do
-        bxor(crc_byte,poly)
-      else
-        bxor(crc_byte,poly)
-      end
+    if ((crc_byte &&& 0x80) != 0) do
+      :math.fmod((crc_byte <<< 1), 256) |> round |> bxor(poly)
     else
-      crc_byte = :math.fmod((crc_byte <<< 1), 256)
-      round(crc_byte)
+      :math.fmod((crc_byte <<< 1), 256) |> round
     end
-    crc8_loop(crc_byte, poly, ref_out, counter + 1)
+    |> crc8_loop(poly, ref_out, counter + 1)
   end
 
-  defp crc8_loop(crc_byte, _, _, 8) do
-    crc_byte
-  end
+  defp crc8_loop(crc_byte, _, _, 8) do crc_byte end
 
-  defp reverce_byte(byte) do
-    :math.fmod((byte * 0x202020202) &&& 0x10884422010, 1023)
-    |> round
-  end
+  defp reverce_byte(byte) do :math.fmod((byte * 0x202020202) &&& 0x10884422010, 1023) |> round end
 end
--- test/crc_test.exs
+++ test/crc_test.exs
@@ -2,15 +2,19 @@ defmodule CrcTest do
   use ExUnit.Case
   doctest CRC
 
-  test "Reverce test" do
-    assert CRC.reverce_byte(0x80) == 0x01
+  def check_string() do
+    "123456789"
   end
 
-  test "crc8 check" do
-    assert CRC.crc8("123456789") == 0xF4
+  test "crc8 test" do
+    assert CRC.crc8(check_string()) == 0xF4
   end
 
-  test "crc8_maxim check" do
-    assert CRC.crc8_maxim("123456789") == 0xA1
+  test "crc8_maxim test" do
+    assert CRC.crc8_maxim(check_string()) == 0xA1
+  end
+
+  test "crc8_cdma2000 test" do
+    assert CRC.crc8_cdma2000(check_string()) == 0xDA
   end
 end
