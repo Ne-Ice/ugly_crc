3bc56193548772fd5b0b51ac75311fdb28bd3898
Nick Lobanov <ne.ice.evil@gmail.com>
2018-04-28 06:03:13 +0300

    no message

--- test/crc_test.exs
+++ test/crc_test.exs
@@ -2,19 +2,17 @@ defmodule CrcTest do
   use ExUnit.Case
   doctest CRC
 
-  def check_string() do
-    "123456789"
-  end
+  @check_string "123456789"
 
   test "crc8 test" do
-    assert CRC.crc8(check_string()) == 0xF4
+    assert CRC.crc8(@check_string) == 0xF4
   end
 
   test "crc8_maxim test" do
-    assert CRC.crc8_maxim(check_string()) == 0xA1
+    assert CRC.crc8_maxim(@check_string) == 0xA1
   end
 
   test "crc8_cdma2000 test" do
-    assert CRC.crc8_cdma2000(check_string()) == 0xDA
+    assert CRC.crc8_cdma2000(@check_string) == 0xDA
   end
 end
